269102836846eee5bb9f98590459a0bee6d83055
Yen <yenchieh86@hotmail.com>
2017-06-09 06:13:37 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  
+  <p [@myAwesomeAnimation] = 'state' (click) = 'x()'>Test</p>
+  
+  `,
+  styles: [`
+  p {
+    width: 200px;
+    background: lightgray;
+    margin: 100px auto;
+    text-align: center;
+    padding: 20px;
+    font-size: 1.5em;
+  }
+  `],
+  
+  animations: [
+    
+    trigger('myAwesomeAnimation', [
+      state('small', style({
+        transform: 'scale(1)',
+      })),
+      state('large',style({
+        transform: 'scale(1.5)',
+      })),
+      
+      transition('small <=> large', animate('300ms ease-in', keyframes([
+        style({ opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+        style({ opacity: 1, transform: 'translateY(35px)', offset: .5}),
+        style({ opacity: 1, transform: 'translateY(0)', offset: 1}),
+      ]))),
+    ]),
+  ]
+  
+  
 })
+
 export class AppComponent {
-  title = 'app';
+  
+  state: string = 'small'
+  
+  x(){
+    this.state = (this.state === 'small'? 'large' : 'small')
+  }
+  
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
+
+export class DataService {
+
+  constructor() { }
+
+    cars = [
+        'Ford', 'Chevrolet', 'Buick'
+    ];
+    
+    myData() {
+        return 'This is my data, man!';
+    }
+
+}
